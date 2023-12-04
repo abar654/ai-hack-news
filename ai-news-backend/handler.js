f73f2c220d3825ed5ea29983d041718813205ed3
abar654 <andrew.barnett.au@gmail.com>
2023-12-04 14:38:05 +0200

    Add GeneratorForm and Result to UI. Update backend to have post generate

--- ai-news-backend/handler.js
+++ ai-news-backend/handler.js
@@ -1,10 +1,39 @@
+const notFound = (response) => {
+    // Send not found response
+    response.writeHead(404, {"Content-Type": "application/json"});
+    response.write(JSON.stringify({ message: "Not found" }));
+    response.end();
+};
+
 const handler = (request, response) => {
 
     const { url, headers, method } = request;
 
-    response.writeHead(500, {"Content-Type": "application/json"});
-    response.write(JSON.stringify({ message: "Hello from the other side", url, method }));
-    response.end();
+    if (method === "POST") {
+        // Extract the body from the request
+        let body = [];
+        request.on("data", (chunk) => {
+            body.push(chunk);
+        }).on("end", () => {
+            body = Buffer.concat(body).toString();
+            console.log(body);
+
+            // Add generate endpoint
+            if (url === "/generate") {
+                response.writeHead(200, {"Content-Type": "application/json"});
+                response.write(JSON.stringify({ message: "Hello from the other side"}));
+                response.end();
+                return;
+            }
+
+            notFound(response);
+            return;
+        });
+        return;
+    }
+
+    notFound(response);
+    return;
 
 };
 
